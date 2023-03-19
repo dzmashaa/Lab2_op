43e505eac958a1fb99730ebf768d13111bca18e0
dzmashaa <124993167+dzmashaa@users.noreply.github.com>
2023-03-19 13:56:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,17 +10,18 @@ int main() {
     cout << "WINTER TIMETABLE:"<< endl;
     printFile(second_file);
     short choice;
-    do {
+    cout << "\nIf you want to add data, press 1";
+    cin >> choice;
+    while(choice == 1){
+        addData(first_file);
+        cout << "TIMETABLE:" << endl;
+        printFile(first_file);
+        createNewFile(first_file, second_file);
+        cout << "WINTER TIMETABLE:" << endl;
+        printFile(second_file);
         cout << "\nIf you want to add data, press 1";
         cin >> choice;
-        if(choice == 1) {
-            addData(first_file);
-            cout << "TIMETABLE:" << endl;
-            printFile(first_file);
-            createNewFile(first_file, second_file);
-            cout << "WINTER TIMETABLE:" << endl;
-            printFile(second_file);
-        }
+    }
     }while(choice == 1);
     return 0;
 }
