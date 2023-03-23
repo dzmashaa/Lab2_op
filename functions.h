999274c7d6a3ab2ec31470dded1ac2e630f7fc01
dzmashaa <124993167+dzmashaa@users.noreply.github.com>
2023-03-23 15:32:05 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -8,12 +8,14 @@ using namespace std;
 struct time;
 struct timetable;
 
-void createFile(string); // створення файлу
-void createNewFile(string, string); // створення нового файлу
-void printRecord(timetable&); // вивід рейсу
-void addData(string); // додати дані в вхідний файл
-void dataInput(ofstream&); // ввід тексту
-int input_minute(string);
-int input_hour(string);
-int input_number();
-void printFile(string);
+void createFile(string);            // створення файлу з розкладом
+void createNewFile(string, string); // створення нового файлу з зимовим розкладом
+void printRecord(timetable&);       // вивід запису
+void addData(string);               // додати дані в вхідний файл
+void dataInput(ofstream&);          // ввід даних
+int input_minute(string);           // ввід хвилин
+int input_hour(string);             // ввід годин
+int input_number(string line);      // ввід номеру потягу
+void printTitle();                  // вивід заголовка
+void printRoute(timetable&);        // вивід маршруту
+void printFile(string);             // вивід файлу
